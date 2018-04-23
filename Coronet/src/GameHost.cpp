1724a1aab01168c9382b1151886f13522badbea8
marika <marika@waifu.club>
2018-04-23 18:40:51 -0300

    Run a Game in Tests.

--- Coronet/src/GameHost.cpp
+++ Coronet/src/GameHost.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include "GameHost.hpp"
 
 namespace Coronet
--- Tests/src/Main.cpp
+++ Tests/src/Main.cpp
@@ -1,4 +1,11 @@
+#include <Coronet/GameHost.hpp>
+
 int main(int argc, char *argv[])
 {
+    auto host = std::make_shared<Coronet::GameHost>();
+    auto game = std::make_shared<Coronet::Game>();
+
+    host->Run(game);
+
     return 0;
 }
