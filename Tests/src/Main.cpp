7a2498253cb1b5387e2d22d52ece91937ab06b87
marika <marika@waifu.club>
2018-05-01 21:53:31 -0300

    Remove asset store testing from Tests/Main.cpp

--- Tests/src/Main.cpp
+++ Tests/src/Main.cpp
@@ -1,10 +1,5 @@
-#include <iostream>
 #include <Coronet/GameHost.hpp>
 #include <Coronet/TestGame.hpp>
-#include <Coronet/AssetStore.hpp>
-#include <Coronet/AssetBitmap.hpp>
-#include <Coronet/AssetTTFFont.hpp>
-#include <SDL2/SDL_image.h>
 
 #include "TestSprite.hpp"
 #include "TestContainer.hpp"
@@ -20,18 +15,6 @@ using namespace Tests;
 
 int main(int argc, char *argv[])
 {
-    auto testStore = Coronet::AssetStore();
-    testStore.Mount("Coronet.assets.zip");
-
-    auto testBitmap = testStore.Read<Coronet::AssetBitmap>("assets/Images/test-selection.png");
-    auto bitmapSize = testBitmap.GetAsset()->GetSize();
-    std::cout << bitmapSize.x << "," << bitmapSize.y << std::endl;
-
-    auto testFont = testStore.Read<Coronet::AssetTTFFont>("assets/Fonts/HelvetiPixel.ttf", 15);
-    auto fontBitmap = testFont.GetAsset()->GetTextBitmap("Text", { 255, 255, 255 });
-    auto fontSize = fontBitmap->GetSize();
-    std::cout << fontSize.x << "," << fontSize.y << std::endl;
-
     auto host = std::make_shared<Coronet::GameHost>("Tests", std::make_shared<Coronet::Metrics>(8, 8, 2, 2, 20, 18));
     auto game = std::make_shared<Coronet::TestGame>();
 
