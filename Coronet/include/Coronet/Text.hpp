0943b5777520fcf21963e49a4e5d8c7f9f99fa86
marika <marika@waifu.club>
2018-05-03 21:13:05 -0300

    Add more ctors for Text to initialize with text and/or colour.

--- Coronet/include/Coronet/Text.hpp
+++ Coronet/include/Coronet/Text.hpp
@@ -13,8 +13,10 @@ namespace Coronet
             SDL_Colour colour; //defaults to white
 
         public:
-            // todo: add partial ctors for quickly setting text/colour
             Text(const std::shared_ptr<Font> &font);
+            Text(const std::shared_ptr<Font> &font, std::string text);
+            Text(const std::shared_ptr<Font> &font, SDL_Colour colour);
+            Text(const std::shared_ptr<Font> &font, std::string text, SDL_Colour colour);
 
             void SetText(std::string text);
             void SetColour(SDL_Colour colour);
--- Coronet/src/Text.cpp
+++ Coronet/src/Text.cpp
@@ -8,6 +8,22 @@ namespace Coronet
         this->colour = { 255, 255, 255 };
     }
 
+    Text::Text(const std::shared_ptr<Font> &font, std::string text) : Text(font)
+    {
+        SetText(text);
+    }
+
+    Text::Text(const std::shared_ptr<Font> &font, SDL_Colour colour) : Text(font)
+    {
+        SetColour(colour);
+    }
+
+    Text::Text(const std::shared_ptr<Font> &font, std::string text, SDL_Colour colour) : Text(font)
+    {
+        SetText(text);
+        SetColour(colour);
+    }
+
     void Text::SetText(std::string text)
     {
         this->text = text;
